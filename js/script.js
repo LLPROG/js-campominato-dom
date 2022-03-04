c9af5fae79246302f70b9b7d47f6fc67cb03849c
LLPROG <94935580+LLPROG@users.noreply.github.com>
2022-03-04 10:08:15 +0000

    bonus

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 
 /*
-Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell'esercizio ma solo l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l'inizializzazione di git).
+    Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell'esercizio ma solo l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l'inizializzazione di git).
 
-L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
-con difficoltà 1 => tra 1 e 100
-con difficoltà 2 => tra 1 e 81
-con difficoltà 3 => tra 1 e 49
+    L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+    con difficoltà 1 => tra 1 e 100
+    con difficoltà 2 => tra 1 e 81
+    con difficoltà 3 => tra 1 e 49
 
-Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+    Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
 
-I numeri nella lista delle bombe non possono essere duplicati.
+    I numeri nella lista delle bombe non possono essere duplicati.
 
-In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+    In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
 
-La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
+    La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
 
-Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una b.
+    Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una b.
 
 */
 
@@ -30,31 +30,26 @@ playButton.addEventListener('click', setupGame);
 function setupGame() {
 
     mainSquare.innerHTML = '';
-
     const indexLevels = parseInt(select.value);
-
     const cellsCount = arrLevels[indexLevels];
-
     const cellParRow = Math.sqrt(cellsCount);
 
     let box;
 
+    // rundom number ciclo
     let arr = [];
-
     let i = 0;
-
-    let allBomb = []
-    let allSave = []
-
-    while (i < 17) {
+    while (i < 16) {
         let randomNumber = Math.floor(Math.random() * (cellsCount - 1) + 1);
         if (!arr.includes(randomNumber)) {
             arr.push(randomNumber)
             i++
         }
     }
+    console.log(arr.sort());
 
-    console.log(arr)
+    let allBomb = [];
+    let allSave = [];
 
     for (let cellNum = 1; cellNum <= cellsCount; cellNum++) {
 
@@ -71,25 +66,44 @@ function setupGame() {
             box.classList.add('box-bomb');
         }
 
-        box.addEventListener('click', changeColor);
+        allBomb = [...document.querySelectorAll('.box-bomb')];
+        allSave = [...document.querySelectorAll('.box-save')];
 
-        allBomb = [...document.querySelectorAll('.box-bomb')]
-        allSave = [...document.querySelectorAll('.box-save')]
-
+        box.addEventListener('click', playGame);
     }
 
-    
+    let boxMoved = [];
 
-    function changeColor() {
+    function playGame() {
 
-        let endGame = document.createElement('div');
-        endGame.classList.add('end-game');
-        endGame.innerHTML = `<div><h3> Hai perso</h3></div>`
+        let endGameLoose = document.createElement('div');
+        endGameLoose.classList.add('loose-game');
+        endGameLoose.innerHTML = `<div><h3> Hai perso</h3></div>`
+
+        let endGameWin = document.createElement('div');
+        endGameWin.classList.add('win-game');
+        endGameWin.innerHTML = `<div><h3> Hai vinto</h3></div>`
 
         if (this.classList.contains('box-save')) {
+
             this.classList.add('selected-save');
+            if (!boxMoved.includes(this)) {
+                boxMoved.push(this)
+                // console.log(boxMoved)
+            }
+
+            if (boxMoved.length == allSave.length) {
+
+                mainSquare.append(endGameWin);
+                for (let i = 0; i < allBomb.length; i++) {
+                allBomb[i].classList.add('selected-bomb');
+                }
+
+                this.removeEventListener('click', playGame());
+            }
+
         } else {
-            mainSquare.append(endGame)
+            mainSquare.append(endGameLoose);
 
             for (let i = 0; i < allBomb.length; i++) {
                 allBomb[i].classList.add('selected-bomb');
@@ -97,11 +111,12 @@ function setupGame() {
 
             for (let i = 0; i < allSave.length; i++) {
                 allSave[i].classList.add('selected-save');
-                
             }
+
+            this.removeEventListener('click', playGame());
+
         }
     }
-     
 
 }
 
